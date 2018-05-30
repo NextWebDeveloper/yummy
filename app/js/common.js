5bced9297165d27c28093e523319f17f687f0897
Dmitry Toshchev <dterron@yandex.ru>
2018-05-30 17:56:07 +0300

    header done

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,48 @@
 $(function() {
 
-	// Custom JS
+	$('.header__menu-button').on('click', function(){
+		var e = $('.header__nav');
+		if ( e.is(':hidden') ) {
+			e.slideDown();
+		} else {
+			e.slideUp();
+		}
+	});
+
+	$('.first-level-link').on('click', function(){
+		$(this).toggleClass('active-color-accent');
+		$('.main-nav__submenu').toggleClass('hidden-show-block');
+	});
+
+	$('.top-slider__slider').slick({
+		arrows: false,
+    infinite: true,
+  	slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+  });
 
 });
