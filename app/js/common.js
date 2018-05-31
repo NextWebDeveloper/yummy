4096d2dc1890fa3db6d6c801a29624feeed8e112
Dmitry Toshchev <dterron@yandex.ru>
2018-05-31 22:11:03 +0300

    footer left

--- app/js/common.js
+++ app/js/common.js
@@ -45,4 +45,35 @@ $(function() {
 		]
   });
 
+	$('.bottom-slider__slider').slick({
+		arrows: false,
+    infinite: true,
+  	slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+  });
+
 });
